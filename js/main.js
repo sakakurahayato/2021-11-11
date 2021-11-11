a2e11c9f4804ada00e1d4d4300f62ceccc579664
sakakuarahayato <sakakura@sakakurasokutonoMacBook-Pro.local>
2021-11-11 16:57:28 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,6 @@
-// const button = document.querySelector("button");
-// button.addEventListener("click",function(){
-//   alert("クリックされました");
-// });
-const button = document.querySelector("button");
-button.addEventListener("click", function () {
-  alert("クリックされました");
-});
-try {
-  console.log("try節:この行は実行されます");
-  undefinedFunction();
-  console.log("try節:この行は実行されません");
-} catch (error) {
-  console.log("catch節:この行は実行されます");
-  console.log(error.message);
-} finally {
-  console.log("finally節:この行は実行されます");
-}
+const menu = document.getElementById('menu');
+menu.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  const drawer = document.getElementById('drawer-menu');
+  drawer.classList.toggle('in');
+})
